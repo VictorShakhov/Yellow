56c3d989656e2bfefb7fba9ff726cd27fc721007
Shakhov Victor <whishnya@gmail.com>
2021-05-20 14:52:50 +0300

    blocks solution accepted

--- integer_types/blocks.cpp
+++ integer_types/blocks.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-    unsigned int N, R, W, H, D;
-    int64_t result = 0, volume = 1;
+    uint N, R, W, H, D;
+    uint64_t result = 0, volume = 1;
     cin >> N >> R;
     for (unsigned int i = 0; i < N; i++)
     {
         cin >> W >> H >> D;
-        volume = volume * W * H * D * R;
-        result += volume;
+        volume = volume * W * H * D;
+        result += volume * R;
         volume = 1;
     }
     cout << result << endl;
